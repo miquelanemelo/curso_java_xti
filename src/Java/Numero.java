1871256c59d8a82c4b19f1ddb38ff4b4530a1cba
Miquelane Melo <miquelanepmelo@gmail.com>
2021-08-23 19:36:46 -0300

    Formatando numeros

--- src/Java/Numero.java
+++ src/Java/Numero.java
@@ -1,6 +1,7 @@
 package Java;
 
 import java.text.NumberFormat;
+import java.util.Locale;
 
 public class Numero {
 
@@ -10,9 +11,24 @@ public class Numero {
 		NumberFormat f = NumberFormat.getInstance(); // formata o numero de acordo com a localização default
 		System.out.println(f.format(saldo));
 		
-		f = NumberFormat.getIntegerInstance();
+		f = NumberFormat.getIntegerInstance(); //Formata o numero apenas com inteiros excluindo a parte fracionada
 		System.out.println(f.format(saldo));
-
+		
+		f = NumberFormat.getPercentInstance(); //Formata o numero em percentuais
+		System.out.println(f.format(0.25));
+		
+		f = NumberFormat.getCurrencyInstance(); //Formata em moeda
+		System.out.println(f.format(saldo));
+		
+		/*Internacionalização*/
+		f = NumberFormat.getCurrencyInstance(Locale.US); //Formata em moeda de acordo com a locale definida
+		System.out.println(f.format(saldo));
+		
+		f = NumberFormat.getCurrencyInstance(Locale.FRANCE); //Formata em moeda de acordo com a locale definida
+		System.out.println(f.format(saldo));
+	
+	
+	
 	}
 
 }
